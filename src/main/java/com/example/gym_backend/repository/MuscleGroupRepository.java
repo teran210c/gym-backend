e573fcd279eb6a95acf3c263a77fef42d0896e11
teran210c <a242729@uach.mx>
2025-12-14 17:29:41 +0000

    Rebuild backend and exercise templates integration

--- src/main/java/com/example/gym_backend/repository/MuscleGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gym_backend.repository;
-
-import com.example.gym_backend.model.MuscleGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MuscleGroupRepository extends JpaRepository<MuscleGroup, Long> {
-}
--- src/main/java/com/example/gym_backend/repository/WeekRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gym_backend.repository;
-
-import com.example.gym_backend.model.Week;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WeekRepository extends JpaRepository<Week, Long> {
-}
